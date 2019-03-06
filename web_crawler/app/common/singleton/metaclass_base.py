94b67970b4db67a899d8e3884c59034b0da2f3b3
vimi <vimiming@gmail.com>
2019-03-06 10:44:22 +0800

    web_crawler common file

---
+++ web_crawler/app/common/singleton/metaclass_base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+class Singleton(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super().__call__(*args, **kwargs)
+        return cls._instance
