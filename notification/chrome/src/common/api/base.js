c2a5c3d41402638d7a4d383f474fbd29c9c44360
vimi <vimiming@gmail.com>
2019-03-15 12:02:39 +0800

    add fetch api

--- notification/chrome/src/common/api/base.js
+++ notification/chrome/src/common/api/base.js
@@ -1,10 +1,61 @@
 import log from '../lib/log';
 
+const OPT = Object.freeze({
+  method: 'GET',
+  mode: 'cors',
+  redirect: 'follow',
+  credentials: 'include',
+  headers: new Headers({
+    'Content-Type': 'text/plain',
+  })
+});
+
+const processResponse = async (response) => {
+  const contentType = response.headers.get('Content-Type');
+  if (!contentType) {
+    throw "no content type";
+  } else {
+    if(contentType.indexOf('text') !== -1) {
+      return await response.text()
+    } else if(contentType.indexOf('form') !== -1) {
+      return await response.formData();
+    } else if(contentType.indexOf('video') !== -1) {
+      return await response.blob();
+    } else if(contentType.indexOf('json') !== -1) {
+      return await response.json();
+    }
+  }
+}
+
+const processOptions = (options) => {
+  return Object.assign(options, OPT);
+}
+
 export default class RequestBase {
   constructor() {
   }
-  get = () => {
-    log.error('use get method');
+
+  doRequest = async (url, options) => {
+    try {
+      const response = await fetch(url, options);
+      if (response.status !== 200) {
+        throw response;
+      } else {
+        return await processResponse(response)
+      }
+    } catch(fetchError) {
+      console.log(fetchError);
+      const error = new Error(fetchError.statusText);
+      error.data = fetchError;
+      // TODO:(weimingliu) add hint messgae for error
+      // throw error;
+    }
+  }
+
+  get = async (url, options = {}) => {
+    const fetchOptions = processOptions(options);
+    fetchOptions.method = 'GET';
+    return await this.doRequest(url, fetchOptions);
   }
   post = () => {}
   put = () => {}
--- notification/chrome/src/common/api/meituan/meishi.js
+++ notification/chrome/src/common/api/meituan/meishi.js
@@ -3,7 +3,11 @@ import RequestBase from '../base';
 class Meishi extends RequestBase {
   constructor() {
     super();
-    this.get();
+    this.getBlog();
+  }
+  getBlog = async () => {
+    const response = await this.get('http://119.23.231.141:8888/blog/get_blog/aaa/1551839688/');
+    console.log(response);
   }
 };
 
