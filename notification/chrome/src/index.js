e709b4df0e6b6e424d0ecc8a49e644e5fb7a05a9
vimi <vimiming@gmail.com>
2019-03-16 22:16:26 +0800

    react router

--- notification/chrome/src/index.js
+++ notification/chrome/src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, Switch} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import Login from './components/login.js';
 import log from './common/lib/log';
 import account from './common/account';
 
-ReactDOM.render(
-  <Login />,
-  document.getElementById('root'),
-);
+class AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Router history={createHistory()}>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="*" component={Login}/>
+        </Switch>
+      </Router>
+    );
+  }
+};
+
+const setupRouter = () => {
+  ReactDOM.render(
+    <AppRouter />,
+    document.getElementById('root'),
+  );
+}
+
+const begin = async () => {
+  try {
+    const result = await account.tryLogin();
+    setupRouter();
+    alert('user in');
+  } catch(sessionTimeoutOrNotSessionError) {
+    setupRouter();
+  }
+}
+
+begin();
