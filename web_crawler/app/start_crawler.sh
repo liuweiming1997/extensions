3808a647460b537812407fa7ee2caf81e141a9d0
vimi <vimiming@gmail.com>
2019-03-25 13:49:43 +0800

    check python3 app/main.py  not python3 because too many

--- web_crawler/app/start_crawler.sh
+++ web_crawler/app/start_crawler.sh
@@ -6,7 +6,7 @@ CHECK_FILE_NAME="check_file_name"
 cd /python3/web_crawler
 echo -n "start doing in ${PWD} ---> "
 
-PID=`ps -aux | grep "python3" | grep -v "grep" | awk '{print $2}'`
+PID=`ps -aux | grep "python3 app/main.py" | grep -v "grep" | awk '{print $2}'`
 
 if [[ ${PID} ]]; then
   echo "PID in ${PID[0]} job exists"
