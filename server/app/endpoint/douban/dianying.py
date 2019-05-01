98ea75c8c3f94bb8464638ed9045117c1aa03a31
vimi <vimiming@gmail.com>
2019-05-01 16:03:56 +0800

    endpoint---> read from redis

--- server/app/endpoint/douban/dianying.py
+++ server/app/endpoint/douban/dianying.py
@@ -10,10 +10,10 @@ dianying_api = flask.Blueprint('/douban/dianying', __name__, url_prefix='/douban
 @parse_user()
 def get_onshow_movie():
     onshow_movie_list = Dianying.get_onshow()
-    return jsonify([onshow_movie.to_json() for onshow_movie in onshow_movie_list])
+    return jsonify(onshow_movie_list)
 
 @dianying_api.route('/upcoming/', methods=['GET'])
 @parse_user()
 def get_upcoming_movie():
     upcoming_movie_list = Dianying.get_upcoming()
-    return jsonify([upcoming_movie.to_json() for upcoming_movie in upcoming_movie_list])
+    return jsonify(upcoming_movie_list)
