90b1538d5046b090dcd4d01e5fd14b92b1a656a9
vimi <vimiming@gmail.com>
2019-05-01 16:07:14 +0800

    revert typo

--- web_crawler/app/main.py
+++ web_crawler/app/main.py
@@ -5,6 +5,6 @@ from request_client.douban.dianying import web_crawler_douban_dianying
 from request_client.medium.programming import web_crawler_medium_programming
 
 if __name__ == '__main__':
-    # web_crawler_meituan_meishi.do()
+    web_crawler_meituan_meishi.do()
     web_crawler_douban_dianying.do()
-    # web_crawler_medium_programming.do()
+    web_crawler_medium_programming.do()
